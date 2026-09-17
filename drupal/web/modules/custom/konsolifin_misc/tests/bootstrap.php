e233e97c89661d0527b6fc799a5b649147766c4e
Jyri Jokinen <jryi@iki.fi>
2026-09-17 20:26:59 +0300

    Added a scheduled content calendar

---
+++ drupal/web/modules/custom/konsolifin_misc/tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Bootstrap for konsolifin_misc PHPUnit tests.
+ */
+
+declare(strict_types=1);
+
+$loader = require dirname(__DIR__, 5) . '/vendor/autoload.php';
+
+// Register the module's PSR-4 namespaces.
+$module_dir = dirname(__DIR__);
+$loader->addPsr4('Drupal\\konsolifin_misc\\', $module_dir . '/src');
+$loader->addPsr4('Drupal\\Tests\\konsolifin_misc\\', $module_dir . '/tests/src');
+
+// Register Drupal core namespaces needed for tests.
+$drupal_root = dirname(__DIR__, 4);
+$loader->addPsr4('Drupal\\Core\\', $drupal_root . '/core/lib/Drupal/Core');
+$loader->addPsr4('Drupal\\Component\\', $drupal_root . '/core/lib/Drupal/Component');
+
+// Register node and user module namespaces.
+$loader->addPsr4('Drupal\\node\\', $drupal_root . '/core/modules/node/src');
+$loader->addPsr4('Drupal\\user\\', $drupal_root . '/core/modules/user/src');
